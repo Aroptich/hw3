bc12dfafe5a772e79f1bbe8d611c78415935f740
Alexsandr <artamoshkinas@gmail.com>
2023-04-23 15:15:36 +0700

    Добавлено: Библиотека prettytable для удобного вывода запроса в консоль. Статический метод чтения запроса с последующим выводом в консоль. Сортировка тадлицы по убыванию/возрастанию.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pymysql
+from prettytable import PrettyTable
 
 from config import host, port, user, password, schema_name
 from employee import employees
@@ -30,6 +31,37 @@ class Database:
 
         return wrapper
 
+    @staticmethod
+    def reading_data(func):
+
+        def wrapper(self, *args, **kwargs):
+            try:
+                connection = pymysql.connect(
+                    host=host,
+                    port=port,
+                    user=user,
+                    password=password,
+                    database=schema_name,
+                    cursorclass=pymysql.cursors.DictCursor
+                )
+                print("successfully connected....")
+                with connection.cursor() as cursor:
+                    cursor.execute(func(self, *args, **kwargs))
+                    x = PrettyTable()
+                    rows = cursor.fetchall()
+                    x.field_names = [row for row in rows[0]]
+                    for row in rows:
+                        x.add_row([row[i] for i in row])
+                    print(x)
+                    connection.commit()
+            except Exception as err:
+                print("Connection refused...")
+                print(err)
+            finally:
+                connection.close()
+
+        return wrapper
+
     @connect
     def create_table(self, table_name: str, **dict_data: dict) -> str:
         try:
@@ -48,8 +80,37 @@ class Database:
             self.table_name = table_name
             self.columns_name = ','.join([keys for keys in data])
             self.values = ','.join([str(data[keys]) for keys in data])
-            self.insert_data_query = f"INSERT INTO {self.table_name} ({self.columns_name}) VALUES ({self.values});"
+            self.insert_data_query = f"INSERT INTO {self.table_name} ({self.columns_name}) \nVALUES ({self.values});"
             return self.insert_data_query
+        except Exception as err:
+            print(err)
+    @reading_data
+    def sort_columns(self, table_name: str, column_name: str, sort=True, limit=None) -> str:
+        try:
+            self.table_name = table_name
+            self.columns_name = column_name
+            if not sort and limit is None or limit == 0:
+                self.sort_columns_query = f"SELECT * \nFROM {self.table_name} \nORDER BY {self.columns_name} DESC"
+                return self.sort_columns_query
+            elif not sort and limit:
+                self.sort_columns_query = f"SELECT * \nFROM {self.table_name} \nORDER BY {self.columns_name} DESC \nLIMIT {limit}"
+                return self.sort_columns_query
+            elif sort and limit > 0:
+                self.sort_columns_query = f"SELECT * \nFROM {self.table_name} \nORDER BY {self.columns_name} \nLIMIT {limit}"
+                return self.sort_columns_query
+            else:
+                self.sort_columns_query = f"SELECT * \nFROM {self.table_name} \nORDER BY {self.columns_name}"
+                return self.sort_columns_query
+
+        except Exception as err:
+            print(err)
+
+    @reading_data
+    def select_data(self, table_name: str, **kwargs):
+        try:
+            self.name_table = table_name
+            self.select_all_rows = f"SELECT * FROM {self.name_table}"
+            return self.select_all_rows
         except Exception as err:
             print(err)
 
@@ -65,5 +126,6 @@ if __name__ == '__main__':
                                    salary='int',
                                    age='int')
     for employee in employees:
-            isert_note = db.insert_data('staff',employee.__dict__)
+        insert_note = db.insert_data('staff', employee.__dict__)
 
+    db.sort_columns('staff', 'salary', sort=True, limit=25)
